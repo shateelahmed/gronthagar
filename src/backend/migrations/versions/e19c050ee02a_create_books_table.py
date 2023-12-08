075a1c1e1be4e4c9a81179e84a15548911356039
Shateel Ahmed <shateelahmed@gmail.com>
2023-12-08 22:36:18 +0600

    feat: add migration to create books table

---
+++ src/backend/migrations/versions/e19c050ee02a_create_books_table.py
@@ -0,0 +1,34 @@
+"""create books table
+
+Revision ID: e19c050ee02a
+Revises:
+Create Date: 2023-12-08 15:39:21.187330
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e19c050ee02a'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "books",
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column('title', sa.String(length=128), nullable=False),
+        sa.Column('authors', sa.String(length=256), nullable=False),
+        sa.Column('content', sa.String(length=512), nullable=False),
+        sa.Column('publication_year', sa.Integer(), nullable=False),
+    )
+    op.create_unique_constraint('uk_books_title', 'books', ['title'])
+
+
+def downgrade() -> None:
+    op.drop_table('books')
