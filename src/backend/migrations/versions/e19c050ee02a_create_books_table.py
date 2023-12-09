eda1ceed4c552518e27d8fb1962b73bd7d44e5e3
Shateel Ahmed <shateelahmed@gmail.com>
2023-12-09 13:10:39 +0600

    fix: update books table migration to add created_at and updated_at columns

--- src/backend/migrations/versions/e19c050ee02a_create_books_table.py
+++ src/backend/migrations/versions/e19c050ee02a_create_books_table.py
@@ -26,6 +26,8 @@ def upgrade() -> None:
         sa.Column('authors', sa.String(length=256), nullable=False),
         sa.Column('content', sa.String(length=512), nullable=False),
         sa.Column('publication_year', sa.Integer(), nullable=False),
+        sa.Column('created_at', sa.DateTime(), server_default=sa.func.current_timestamp(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), server_default=sa.func.current_timestamp(), nullable=False),
     )
     op.create_unique_constraint('uk_books_title', 'books', ['title'])
 
