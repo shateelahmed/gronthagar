f9066f6193e4e8bc9bc6e3624d527a14b0f5fd56
Shateel Ahmed <shateelahmed@gmail.com>
2023-12-12 00:18:35 +0600

    chore: add tests for main

---
+++ src/backend/app/test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": f"Welcome to {app.title}"}
+
+def test_seed():
+    response = client.get("/seed")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Seeding successful",
+        "data": None
+    }
