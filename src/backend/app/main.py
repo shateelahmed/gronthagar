4cd2505ede279cdba14810b47172d7d62772ba31
Shateel Ahmed <shateelahmed@gmail.com>
2023-12-11 23:07:49 +0600

    refactor: implement book router

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI
 from app.models import database, Book
-from ormar import NoMatch
 from fastapi.middleware.cors import CORSMiddleware
+from .routers import books
 
 app = FastAPI(title="Gronthagar")
 
@@ -30,8 +30,7 @@ async def shutdown():
     if database.is_connected:
         await database.disconnect()
 
-def format_exception(exception: Exception) -> str:
-    return f"An exception occurred. Exception type: {type(exception).__name__}. Arguments: {','.join(map(str, exception.args))}"
+app.include_router(books.router)
 
 @app.get("/")
 async def read_root():
@@ -49,131 +48,4 @@ async def seed():
     return {
         "message": "Seeding successful",
         "data": None
-    }
-
-@app.get("/api/v1/books")
-async def get_books(response: Response, q: str | None = None) -> Response:
-    response.status_code = status.HTTP_404_NOT_FOUND
-    message = "No books found"
-    books = []
-
-    try:
-        if q:
-            books = await database.fetch_all(
-                """
-                    SELECT
-                        id,
-                        title,
-                        authors,
-                        summary,
-                        publication_year
-                    FROM
-                        books
-                    WHERE
-                        ts @@ plainto_tsquery(:q)
-                """,
-                {
-                    'q': q
-                }
-            )
-        else:
-            books = await Book.objects.all()
-
-        # books = await Book.objects.filter(
-        #     ormar.or_(
-        #         title__icontains=q,
-        #         authors__icontains=q,
-        #         summary__icontains=q
-        #     )
-        # ).get()
-        if books:
-            response.status_code = status.HTTP_200_OK
-            message = "Books fetched successfully"
-    except Exception as exception:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        message = format_exception(exception)
-
-    return {
-        "message": message,
-        "data": books
-    }
-
-@app.get("/api/v1/books/{id}")
-async def get_book(id: int, response: Response) -> Response:
-    book = {}
-
-    try:
-        book = await Book.objects.get(pk=id)
-        response.status_code = status.HTTP_200_OK
-        message = "Book fetched successfully"
-    except NoMatch as ex:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        message = "No book found"
-    except Exception as exception:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        message = format_exception(exception)
-
-    return {
-        "message": message,
-        "data": book
-    }
-
-@app.post("/api/v1/books")
-async def create_book(book: Book, response: Response) -> Response:
-    try:
-        book = await book.save()
-        response.status_code = status.HTTP_201_CREATED
-        message = "Book created successfully"
-    except Exception as exception:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        message = format_exception(exception)
-        book = {}
-
-    return {
-        "message": message,
-        "data": book
-    }
-
-@app.put("/api/v1/books/{id}")
-async def update_book(id: int, book: Book, response: Response) -> Response:
-    try:
-        book_from_db = await Book.objects.get(pk=id)
-        book.id = id
-        book = await book_from_db.update(**book.dict())
-        response.status_code = status.HTTP_200_OK
-        message = "Book updated successfully"
-    except NoMatch as ex:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        message = f"Invalid book ID {id}"
-        book = {}
-    except Exception as exception:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        message = format_exception(exception)
-        book = {}
-
-    return {
-        "message": message,
-        "data": book
-    }
-
-@app.delete("/api/v1/books/{id}")
-async def delete_book(id: int, response: Response) -> Response:
-    book = {}
-
-    try:
-        book_from_db = await Book.objects.get(pk=id)
-        await book_from_db.delete()
-        response.status_code = status.HTTP_200_OK
-        message = "Book deleted successfully"
-        book = book_from_db
-    except NoMatch as ex:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        message = f"Invalid book ID {id}"
-    except Exception as exception:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        message = format_exception(exception)
-
-    return {
-        "message": message,
-        "data": book
     }
---
+++ src/backend/app/routers/books.py
@@ -0,0 +1,138 @@
+from fastapi import APIRouter, Response, status
+from app.models import database, Book
+from ormar import NoMatch
+
+router = APIRouter(
+    prefix="/api/v1/books",
+    tags=["books"],
+)
+
+def format_exception(exception: Exception) -> str:
+    return f"An exception occurred. Exception type: {type(exception).__name__}. Arguments: {','.join(map(str, exception.args))}"
+
+@router.get("/")
+async def get_books(response: Response, q: str | None = None) -> Response:
+    response.status_code = status.HTTP_404_NOT_FOUND
+    message = "No books found"
+    books = []
+
+    try:
+        if q:
+            books = await database.fetch_all(
+                """
+                    SELECT
+                        id,
+                        title,
+                        authors,
+                        summary,
+                        publication_year
+                    FROM
+                        books
+                    WHERE
+                        ts @@ plainto_tsquery(:q)
+                """,
+                {
+                    'q': q
+                }
+            )
+        else:
+            books = await Book.objects.all()
+
+        # books = await Book.objects.filter(
+        #     ormar.or_(
+        #         title__icontains=q,
+        #         authors__icontains=q,
+        #         summary__icontains=q
+        #     )
+        # ).get()
+        if books:
+            response.status_code = status.HTTP_200_OK
+            message = "Books fetched successfully"
+    except Exception as exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        message = format_exception(exception)
+
+    return {
+        "message": message,
+        "data": books
+    }
+
+@router.get("/{id}")
+async def get_book(id: int, response: Response) -> Response:
+    book = {}
+
+    try:
+        book = await Book.objects.get(pk=id)
+        response.status_code = status.HTTP_200_OK
+        message = "Book fetched successfully"
+    except NoMatch as ex:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        message = "No book found"
+    except Exception as exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        message = format_exception(exception)
+
+    return {
+        "message": message,
+        "data": book
+    }
+
+@router.post("/")
+async def create_book(book: Book, response: Response) -> Response:
+    try:
+        book = await book.save()
+        response.status_code = status.HTTP_201_CREATED
+        message = "Book created successfully"
+    except Exception as exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        message = format_exception(exception)
+        book = {}
+
+    return {
+        "message": message,
+        "data": book
+    }
+
+@router.put("/{id}")
+async def update_book(id: int, book: Book, response: Response) -> Response:
+    try:
+        book_from_db = await Book.objects.get(pk=id)
+        book.id = id
+        book = await book_from_db.update(**book.dict())
+        response.status_code = status.HTTP_200_OK
+        message = "Book updated successfully"
+    except NoMatch as ex:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        message = f"Invalid book ID {id}"
+        book = {}
+    except Exception as exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        message = format_exception(exception)
+        book = {}
+
+    return {
+        "message": message,
+        "data": book
+    }
+
+@router.delete("/{id}")
+async def delete_book(id: int, response: Response) -> Response:
+    book = {}
+
+    try:
+        book_from_db = await Book.objects.get(pk=id)
+        await book_from_db.delete()
+        response.status_code = status.HTTP_200_OK
+        message = "Book deleted successfully"
+        book = book_from_db
+    except NoMatch as ex:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        message = f"Invalid book ID {id}"
+    except Exception as exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        message = format_exception(exception)
+
+    return {
+        "message": message,
+        "data": book
+    }
