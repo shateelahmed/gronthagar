dbef4e52d260b8aa715230e5e00bb2297cc985ad
Shateel Ahmed <shateelahmed@gmail.com>
2023-12-17 19:19:41 +0600

    chore: add test cases for validation errors

--- src/backend/app/tests/test_books.py
+++ src/backend/app/tests/test_books.py
@@ -3,7 +3,7 @@ from ..main import app, seed
 
 client = TestClient(app)
 
-seed()
+# await seed()
 
 def test_get_books():
     response = client.get("/api/v1/books")
@@ -24,6 +24,16 @@ def test_create_book():
     print(response.json())
     assert response.status_code == 201
 
+def test_create_book_with_invalid_payload():
+    data = {
+        "title": "Harry Potter And The Prisoner of Azkaban",
+        "authors": "J. K. Rowling",
+        "summary": "The content of Harry Potter And The Prisoner of Azkaban by J. K. Rowling",
+    }
+    response = client.post("/api/v1/books", json=data)
+    print(response.json())
+    assert response.status_code == 422
+
 def test_update_book():
     data = {
         "title": "Harry Potter And The Prisoner of Azkaban",
@@ -35,7 +45,22 @@ def test_update_book():
     print(response)
     assert response.status_code == 200
 
+def test_update_book_invalid_payload():
+    data = {
+        "title": "Harry Potter And The Prisoner of Azkaban",
+        "authors": "J. K. Rowling",
+        "publication_year": 2001
+    }
+    response = client.put("/api/v1/books/1", json=data)
+    print(response)
+    assert response.status_code == 422
+
 def test_delete_book():
     response = client.delete("/api/v1/books/1")
     print(response)
     assert response.status_code == 200
+
+def test_delete_book_invalid_book():
+    response = client.delete("/api/v1/books/2")
+    print(response)
+    assert response.status_code == 404
