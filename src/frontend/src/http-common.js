3d0a5f8a305ce0d5f62c98c75548651bfaa8883c
Shateel Ahmed <shateelahmed@gmail.com>
2023-12-17 11:37:49 +0600

    refactor: add ports to env

--- src/frontend/src/http-common.js
+++ src/frontend/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create(
     {
         // baseURL: process.env.API_BASE_URL,
-        baseURL: "http://localhost:8000/api/v1",
+        baseURL: `http://localhost:8000/api/v1`,
         headers: {
             "Content-type": "application/json"
         }
